e7f1ae24404794a1ef7836fe15c7ba9c6dc72bec
William Stock <grim118@hotmail.com>
2022-06-04 19:20:54 -0500

    set up user model except for the virtual

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    }
+);
+
+// TODO set up user virtual to retrieve length of user's friends array on query
+UserSchema.virtual('friendCount').get(function() {
+
+});
+
+
+const User = model('User', UserSchema);
+
+module.exports = User;
