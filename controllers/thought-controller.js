abbaa5527b797214336890b41ff15056082f4600
William Stock <grim118@hotmail.com>
2022-06-12 13:10:49 -0500

    fixed deleteThought controller function

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -77,26 +77,11 @@ const thoughtController = {
     },
 
     removeThought({ params }, res) {
-        Thought.findOneAndDelete({ _id: params.thoughtId })
-            .then(deletedThought => {
-                if (!deletedThought) {
-                    return res.status(404).json({ message: 'No thought with this id! '});
-                }
-                return User.findOneAndUpdate(
-                    { _id: params.userId },
-                    { $pull: { thoughts: params.thoughtId } },
-                    { new: true }
-                );
-            })
-            .then(dbUserData => {
-                if (!dbUserData) {
-                    res.status(404).json({ message: 'No user found with this id! '});
-                    return;
-                }
-                res.json(dbUserData);
-            })
+        Thought.findOneAndDelete({ _id: params.id })
+            .then(deletedThought => res.json(deletedThought))
             .catch(err => res.json(err));
     },
+    
 
     removeReaction({ params }, res) {
         Thought.findOneAndUpdate(
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
 
 const {
+    getAllthoughts,
+    getThoughtById,
     addThought,
+    updateThought,
     addReaction,
     removeThought,
     removeReaction
 } = require('../../controllers/thought-controller');
+
+
+router
+    .route('/')
+    .get(getAllthoughts);
+
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(removeThought);
+    
 
 
 router.route('/:userId').post(addThought);
@@ -13,8 +29,9 @@ router.route('/:userId').post(addThought);
 
 router
     .route('/:userId/:thoughtId')
-    .put(addReaction)
-    .delete(removeThought);
+    .put(addReaction);
+    
+    
 
 
 router.route('/:userId/:thoughtId/:reactionId').delete(removeReaction);
